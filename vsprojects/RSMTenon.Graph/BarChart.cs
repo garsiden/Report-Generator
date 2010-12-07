85d222ff8068ac32c1c8c9ed5274f5165dd8b5f9
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-07 12:16:23 +0000

    Change library name to avoid collision with DocumentFormat classes.
    git-svn-id: svn://localhost/vantis/repgen/trunk@61 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/RSMTenon.Graph/BarChart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using DocumentFormat.OpenXml.Drawing.Wordprocessing;
+
+
+
+namespace RSMTenon.ReportGenerator.Graph
+{
+    class BarGraph : Graph
+    {
+
+
+    }
+}
