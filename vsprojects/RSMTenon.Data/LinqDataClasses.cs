b210bc306f1e5af6bd682146b924a1741e9a2bfd
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-10 17:35:59 +0000

    New data handling library.
    git-svn-id: svn://localhost/vantis/repgen/trunk@85 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/RSMTenon.Data/LinqDataClasses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSMTenon.Data
+{
+    // Model allocation weightings
+    public class ModelAllocation
+    {
+        public string InvestmentType { get; set; }
+        public decimal Allocation { get; set; }
+    }
+}
