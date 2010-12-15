a14ac7e06833eae7ab8e487921055c93f9b400b2
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-15 22:15:09 +0000

    Changes to test console application
    git-svn-id: svn://localhost/vantis/repgen/trunk@96 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/word_chart/Program.cs
+++ vsprojects/word_chart/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Linq;
 using System.Text;
 using System.IO;
 using DocumentFormat.OpenXml;
@@ -9,6 +10,8 @@ using DocumentFormat.OpenXml.Wordprocessing;
 using Wp = DocumentFormat.OpenXml.Drawing.Wordprocessing;
 using A = DocumentFormat.OpenXml.Drawing;
 using C = DocumentFormat.OpenXml.Drawing.Charts;
+using System.Threading;
+
 using RSMTenon.Graphing;
 using RSMTenon.Data;
 
@@ -18,21 +21,24 @@ namespace RSMTenon.ReportGenerator
     {
         static string path = @"C:\Documents and Settings\garsiden\My Documents\Projects\RepGen\test\chart\";
         static string generated = path + "ChartDoc.docx";
+        private double previousDD = 1;
 
         static void Main(string[] args)
         {
             Program This = new Program();
-            var myData = new Dictionary<string, decimal>();
-            myData.Add("Cash", 0.015M);
-            myData.Add("Fixed Interest", 0.452M);
-            myData.Add("Hedge", 0.14M);
-            myData.Add("Real Estate", 0.15M);
-            myData.Add("Long Short Equities", 0.12M);
-            myData.Add("Long Equities", 0.09M);
-            myData.Add("Commodities", 0.033M);
+
+            // create test Client and Report
+            Client client = new Client() { Name = "Joe Mayo", ExistingAssets = false, MeetingDate = new DateTime(2010, 12, 14), StrategyId = "CO", TimeHorizon = 5, Investment = 1000000 };
+            Report report = new Report() { Client = client };
+
+            //This.PriceTest();
+            //This.RollingReturnTest();
+            //This.DrawdownTest();
+            //Thread.Sleep(500000);
+            //return;
 
             string template = path + "chart_template.docx";
-            string generated = path + "PieChartWord.docx";
+            string generated = path + "chart_test.docx";
 
             // copy template to generated
             File.Copy(template, generated, true);
@@ -41,12 +47,100 @@ namespace RSMTenon.ReportGenerator
             WordprocessingDocument myWordDoc = WordprocessingDocument.Open(generated, true);
             MainDocumentPart mainPart = myWordDoc.MainDocumentPart;
 
-            //This.InsertLineChartIntoWord(mainPart, "1yr Rolling return", myData);
             This.InsertAllocationPie(mainPart);
+            This.InsertRRChartIntoWord(mainPart, report);
 
             myWordDoc.Close();
         }
 
+        private void RollingReturnTest()
+        {
+            var ctx = new RepGenDataContext();
+            ReturnCalculation calc = new ReturnCalculation();
+
+            // calculate prices
+            ctx.ModelPrice("CO");
+            var data = ctx.ModelPrice("CO");
+
+            var prices = from d in data
+                         select new ReturnData {
+                             Value = calc.calculatePrice(d),
+                             Date = d.Date
+                         };
+
+            var p1 = prices.ToList();
+            p1.Insert(0, new ReturnData() { Value = 100D });
+            var p2 = p1.Skip(12 * 3);
+            var p3 = new List<ReturnData>();
+            int i = 0;
+
+            foreach (var item in p2) {
+                var rd = new ReturnData {
+                    Value = calc.calculateRollingReturn(item, p1.ElementAt(i++)),
+                    Date = item.Date
+                };
+                p3.Add(rd);
+            }
+
+            foreach (var item in p3) {
+                Console.WriteLine("{0}\t{1}", item.Date, item.Value);
+            }
+
+
+        }
+
+        private void PriceTest()
+        {
+
+            var ctx = new RepGenDataContext();
+            ctx.ModelPrice("CO");
+            var data = ctx.ModelPrice("CO");
+
+            ReturnCalculation calc = new ReturnCalculation();
+            var match = from d in data
+                        select new ReturnData {
+                            Value = calc.calculatePrice(d),
+                            Date = d.Date
+                        };
+
+            foreach (var item in match) {
+                Console.WriteLine("{0}\t{1}", item.Date, item.Value);
+            }
+            Thread.Sleep(500000);
+
+        }
+
+        private void DrawdownTest()
+        {
+
+            var ctx = new RepGenDataContext();
+            var data = ctx.Drawdown("UKGB");
+
+            var match = from d in data
+                        select new ReturnData {
+                            Value = calculateDrawdown(d),
+                            Date = d.Date
+                        };
+
+            foreach (var item in match) {
+                Console.WriteLine("{0}\t{1}", item.Date, item.Value);
+            }
+            Thread.Sleep(500000);
+
+        }
+
+        private double calculateDrawdown(Drawdown drawdown)
+        {
+            if ((drawdown.Value / drawdown.PreviousValue > 1) && previousDD == 1) {
+                return 1D;
+            } else {
+                double retval = Math.Min(1D, previousDD * (1 + Math.Log(drawdown.Value / drawdown.PreviousValue)));
+                this.previousDD = retval;
+                return retval;
+            }
+
+
+        }
         public void InsertBarChartIntoWord(MainDocumentPart mainPart, string title)
         {
             // open Word documant and remove existing content from control
@@ -80,7 +174,12 @@ namespace RSMTenon.ReportGenerator
 
         public void InsertAllocationPie(MainDocumentPart mainPart)
         {
-            Paragraph para = findAndRemoveContent(mainPart, "Chart1");
+            // variables from client details
+            bool assets = false;
+            string strategyId = "CO";
+            string strategyName = Strategy.GetStrategyNameFromId(strategyId);
+
+            Paragraph para = findAndRemoveContent(mainPart, "AllocationPieChart");
 
             // generate new ChartPart and ChartSpace
             ChartPart chartPart = mainPart.AddNewPart<ChartPart>();
@@ -88,21 +187,16 @@ namespace RSMTenon.ReportGenerator
             C.ChartSpace chartSpace = GraphSpace.GenerateChartSpace(chartPart);
 
             // generate Pie Chart and add to ChartSpace
-            string title = "Conservative Allocation";
+            string title = strategyName + " Allocation";
+            IQueryable<ModelAllocation> allocation;
 
-            // get data
-            var context = new RepGenDataContext();
-            context.Connection.Open();
-            var models = context.StrategyModels;
-
-            var match = from model in models
-                        where model.StrategyID.Equals("CO")
-                        group model by model.InvestmentTypeName into g
-                        select new ModelAllocation{InvestmentType = g.Key, Allocation = g.Sum(model => model.Weighting)};
-
-
+            if (assets) {
+                allocation = Model.GetModelAllocation("CO");
+            } else {
+                allocation = Model.GetModelAllocation("CO");
+            }
             AllocationPieChart pie = new AllocationPieChart();
-            C.Chart chart = pie.GenerateChart(title, match);
+            C.Chart chart = pie.GenerateChart(title, allocation.ToList());
             chartSpace.Append(chart);
 
             // set ChartPart ChartSpace
@@ -111,7 +205,7 @@ namespace RSMTenon.ReportGenerator
             // generate a new Wordprocessing Drawing, add to a new Run,
             // and relate to new ChartPart
             Run run = new Run();
-            Drawing drawing = GraphDrawing.GenerateDrawing(relId, "Chart 1", 2U, AllocationPieChart.Cx, AllocationPieChart.Cy);
+            Drawing drawing = GraphDrawing.GenerateDrawing(relId, title, 2U, AllocationPieChart.Cx, AllocationPieChart.Cy);
             para.Append(run);
             run.Append(drawing);
 
@@ -119,20 +213,25 @@ namespace RSMTenon.ReportGenerator
             mainPart.Document.Save();
         }
 
-        public void InsertLineChartIntoWord(MainDocumentPart mainPart, string title)
+        public void InsertRRChartIntoWord(MainDocumentPart mainPart, Report report)
         {
+            // variables from client details
+            //string strategyName = Strategy.GetStrategyNameFromId(report.Client.StrategyId);
+            //string title = "1yr Rolling return";
+            //string contentControlName = "RollingReturnOneYear";
+            ChartItem chartItem = report.RollingReturnChart(3);
+            string ccn = chartItem.CustomControlName;
+
             // open Word documant and remove existing content from control
-            Paragraph para = findAndRemoveContent(mainPart, "Chart1");
+            Paragraph para = findAndRemoveContent(mainPart, ccn);
 
             // generate new ChartPart and ChartSpace
             ChartPart chartPart = mainPart.AddNewPart<ChartPart>();
             string relId = mainPart.GetIdOfPart(chartPart);
             C.ChartSpace chartSpace = GraphSpace.GenerateChartSpace(chartPart);
 
-            // generate Line Chart and add to ChartSpace
-            DrawdownLineChart lc = new DrawdownLineChart();
-            C.Chart chart = lc.GenerateChart(title);
-            chartSpace.Append(chart);
+
+            chartSpace.Append(chartItem.Chart);
 
             // set ChartPart ChartSpace
             chartPart.ChartSpace = chartSpace;
@@ -140,7 +239,7 @@ namespace RSMTenon.ReportGenerator
             // generate a new Wordprocessing Drawing, add to a new Run,
             // and relate to new ChartPart
             Run run = new Run();
-            Drawing drawing = GraphDrawing.GenerateDrawing(relId, "Chart 2", 3U, RollingReturnLineChart.Cx, RollingReturnLineChart.Cy);
+            Drawing drawing = GraphDrawing.GenerateDrawing(relId, ccn, 3U, RollingReturnLineChart.Cx, RollingReturnLineChart.Cy);
             para.Append(run);
             run.Append(drawing);
 
