8a408fdaca1cdf8c94e22de71cc7af6fd471c8d2
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-16 19:22:55 +0000

    Data tables added to DMBL file
    git-svn-id: svn://localhost/vantis/repgen/trunk@97 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/RSMTenon.Data/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSMTenon.Data
+{
+    public partial class Client
+    {
+
+        public static Client GetClientByGUID(Guid clientGuid)
+        {
+            var ctx = new RepGenDataContext();
+
+            return ctx.Clients.Single(c => c.GUID == clientGuid);
+        }
+    }
+}
---
+++ vsprojects/RSMTenon.Data/ClientAssetClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSMTenon.Data
+{
+
+    public partial class ClientAssetClass
+    {
+
+        public static IQueryable<AssetWeighting> GetClientAssetClass(Guid clientGuid)
+        {
+            var ctx = new RepGenDataContext();
+            var assets = ctx.ClientAssetClasses;
+
+            var weighting = from asset in assets
+                            where asset.ClientGUID.Equals(clientGuid)
+                            //group asset by asset.AssetClass.Name  into g
+                            //select new AssetWeighting { AssetClass = g.Key, Allocation = g.Sum(asset => asset.Weighting) };
+                            select new AssetWeighting { AssetClass = asset.AssetClass.Name, Weighting = asset.Weighting };
+            return weighting;
+
+        }
+    }
+}
