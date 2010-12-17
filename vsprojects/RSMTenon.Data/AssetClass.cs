70f5a9bbf6f1bf221855e2031f91f3438384be20
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-17 12:00:41 +0000

    New class added
    git-svn-id: svn://localhost/vantis/repgen/trunk@107 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/RSMTenon.Data/AssetClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSMTenon.Data
+{
+    public partial class AssetClass
+    {
+        public string ColourHex { get; set; }
+    }
+}
