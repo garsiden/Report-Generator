2a7e440d8efb17064890d31f5ad40f48825a3394
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-18 12:45:31 +0000

    Use base class methods to create chart
    git-svn-id: svn://localhost/vantis/repgen/trunk@111 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/RSMTenon.Graphing/TenYearLineChart.cs
+++ vsprojects/RSMTenon.Graphing/TenYearLineChart.cs
@@ -11,7 +11,77 @@ namespace RSMTenon.Graphing
 {
     public class TenYearLineChart  : LineGraph
     {
+        public TenYearLineChart()
+        {
+            this.axisFormat = "mmm\\-yy";
+            this.valueFormat = "0.0%";
+            this.dateAxisFormat = "mmm\\-yy";
+            this.valueAxisFormat = "0%";
+        }
+
         // Creates an Chart instance and adds its children.
+        public Chart GenerateChart(string title)
+        {
+            // c:chart (Chart)
+            Chart chart1 = new Chart();
+
+            // c:title (Title)
+            if (title != null) {
+                Title title1 = GenerateTitle(title, TITLE_FONT_SIZE);
+                chart1.Append(title1);
+            } else {
+                AutoTitleDeleted autoTitleDeleted1 = new AutoTitleDeleted() { Val = true };
+                chart1.Append(autoTitleDeleted1);
+            }
+
+            // c:plotArea (PlotArea)
+            PlotArea plotArea1 = new PlotArea();
+            // c:layout (Layout)
+            Layout layout2 = new Layout();
+
+            // c:lineChart (LineChart)
+            LineChart lineChart1 = new LineChart();
+            // c:grouping (Grouping)
+            Grouping grouping1 = new Grouping() { Val = GroupingValues.Standard };
+
+            // c:marker (Marker)
+            ShowMarker showMarker1 = new ShowMarker() { Val = true };
+            // c:axId (AxisId)
+            AxisId axisId1 = new AxisId() { Val = (UInt32Value)132034944U };
+            AxisId axisId2 = new AxisId() { Val = (UInt32Value)132036480U };
+
+            lineChart1.Append(grouping1);
+            //lineChart1.Append(lineChartSeries1);
+            lineChart1.Append(showMarker1);
+            lineChart1.Append(axisId1);
+            lineChart1.Append(axisId2);
+
+            // c:dateAx (DateAxis)
+            DateAxis dateAxis1 = GenerateDateAxis(axisId1, AxisPositionValues.Bottom, axisId2, TickLabelPositionValues.Low);
+
+            // c:valAx (ValueAxis)
+            ValueAxis valueAxis1 = GenerateValueAxis(axisId2, AxisPositionValues.Left, axisId1);
+
+            // c:plotArea (PlotArea)
+            plotArea1.Append(layout2);
+            plotArea1.Append(lineChart1);
+            plotArea1.Append(dateAxis1);
+            plotArea1.Append(valueAxis1);
+
+            // c:legend (Legend)
+            Legend legend1 = GenerateLegend(LegendPositionValues.Bottom);
+
+            // c:plotVisOnly (PlotVisibleOnly)
+            PlotVisibleOnly plotVisibleOnly1 = new PlotVisibleOnly() { Val = true };
+
+
+            chart1.Append(plotArea1);
+            chart1.Append(legend1);
+            chart1.Append(plotVisibleOnly1);
+
+            return chart1;
+        }
+
         public Chart GenerateChart()
         {
             Chart chart1 = new Chart();
@@ -21,24 +91,24 @@ namespace RSMTenon.Graphing
 
             Layout layout1 = new Layout();
 
-            ManualLayout manualLayout1 = new ManualLayout();
-            LayoutTarget layoutTarget1 = new LayoutTarget(){ Val = LayoutTargetValues.Inner };
-            LeftMode leftMode1 = new LeftMode(){ Val = LayoutModeValues.Edge };
-            TopMode topMode1 = new TopMode(){ Val = LayoutModeValues.Edge };
-            Left left1 = new Left(){ Val = 0.11843285214348206D };
-            Top top1 = new Top(){ Val = 5.1298768155777226E-2D };
-            Width width1 = new Width(){ Val = 0.85101159230096224D };
-            Height height1 = new Height(){ Val = 0.58327109049811265D };
+            //ManualLayout manualLayout1 = new ManualLayout();
+            //LayoutTarget layoutTarget1 = new LayoutTarget(){ Val = LayoutTargetValues.Inner };
+            //LeftMode leftMode1 = new LeftMode(){ Val = LayoutModeValues.Edge };
+            //TopMode topMode1 = new TopMode(){ Val = LayoutModeValues.Edge };
+            //Left left1 = new Left(){ Val = 0.11843285214348206D };
+            //Top top1 = new Top(){ Val = 5.1298768155777226E-2D };
+            //Width width1 = new Width(){ Val = 0.85101159230096224D };
+            //Height height1 = new Height(){ Val = 0.58327109049811265D };
 
-            manualLayout1.Append(layoutTarget1);
-            manualLayout1.Append(leftMode1);
-            manualLayout1.Append(topMode1);
-            manualLayout1.Append(left1);
-            manualLayout1.Append(top1);
-            manualLayout1.Append(width1);
-            manualLayout1.Append(height1);
+            //manualLayout1.Append(layoutTarget1);
+            //manualLayout1.Append(leftMode1);
+            //manualLayout1.Append(topMode1);
+            //manualLayout1.Append(left1);
+            //manualLayout1.Append(top1);
+            //manualLayout1.Append(width1);
+            //manualLayout1.Append(height1);
 
-            layout1.Append(manualLayout1);
+            //layout1.Append(manualLayout1);
 
             LineChart lineChart1 = new LineChart();
             Grouping grouping1 = new Grouping(){ Val = GroupingValues.Standard };
@@ -88,8 +158,6 @@ namespace RSMTenon.Graphing
             CategoryAxisData categoryAxisData1 = new CategoryAxisData();
 
             NumberReference numberReference1 = new NumberReference();
-            Formula formula2 = new Formula();
-            formula2.Text = "\'Client RR and DD\'!$FI$7:$FI$139";
 
             NumberingCache numberingCache1 = new NumberingCache();
             FormatCode formatCode1 = new FormatCode();
