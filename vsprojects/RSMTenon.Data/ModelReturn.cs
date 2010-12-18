96b8029d17dc49adaf4227e2d537c686c4ebd05c
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-18 16:01:46 +0000

    Extend class to continue use of single argument to underlying stored procedure
    git-svn-id: svn://localhost/vantis/repgen/trunk@121 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/RSMTenon.Data/ModelReturn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Reflection;
+
+namespace RSMTenon.Data
+{
+    public partial class RepGenDataContext
+
+    {
+        [Function(Name = "dbo.spModelReturn")]
+        public ISingleResult<ReturnData> ModelReturn([Parameter(DbType = "Char(2)")] string strategyId)
+        {
+            IExecuteResult result = this.ExecuteMethodCall(this, ((MethodInfo)(MethodInfo.GetCurrentMethod())), strategyId, null);
+            return ((ISingleResult<ReturnData>)(result.ReturnValue));
+        }
+
+
+    }
+}
