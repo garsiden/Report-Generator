e7b63f4684b1fa982c99c87f2351e1648745c3b9
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-19 21:10:36 +0000

    Remove test code
    git-svn-id: svn://localhost/vantis/repgen/trunk@129 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/RSMTenon.Graphing/StressTestBarChart.cs
+++ vsprojects/RSMTenon.Graphing/StressTestBarChart.cs
@@ -8,8 +8,6 @@ using DocumentFormat.OpenXml;
 
 namespace RSMTenon.Graphing
 {
-
-
     public class StressTestBarChart : BarGraph
     {
         private string pointFormat = "0.00%";
@@ -18,74 +16,6 @@ namespace RSMTenon.Graphing
         {
             BarChartSeries barChartSeries = GenerateBarChartSeries(series.Name, series.PointNames, series.Values, series.ColourHex, pointFormat);
             barChart.PlotArea.ChildElements.First<BarChart>().Append(barChartSeries);
-        }
-
-        public Chart GenerateChartCrash(string title)
-        {
-            float[] global = { -0.15650000000000006F, -0.28730000000000011F, -0.2782F };
-            float[] bonds = { 6.9600000000000023E-2F, 8.2300000000000012E-2F, 9.3000000000000055E-2F };
-            float[] strategy = { 7.9000000000000042E-3F, 1.7100000000000001E-2F, 0.10600000000000002F };
-
-            string[] pointNames = {"Russian Debt Crisis & LTCM Collapse Aug 98 - Sep 98",
-                                      "Technology Bubble Bursting Apr 00 - Sep 01",
-                                        "Economic Slowdown May 02 - Sep 02"};
-            Chart chart1 = new Chart();
-            Title title1 = GenerateTitle("Stress Test - Market Crashes");
-            PlotArea plotArea1 = new PlotArea();
-
-            Layout layout2 = GeneratePlotAreaLayout();
-
-            BarChart barChart1 = new BarChart();
-            BarDirection barDirection1 = new BarDirection() { Val = BarDirectionValues.Column };
-            BarGrouping barGrouping1 = new BarGrouping() { Val = BarGroupingValues.Clustered };
-
-            //BarChartSeries barChartSeries1 = GenerateBarChartSeries("Global Equities", 1U, 0U, pointNames, global, "808080", "0.00%");
-            //BarChartSeries barChartSeries2 = GenerateBarChartSeries("UK Government Bonds", 2U, 1U, pointNames, bonds, "C0C0C0", "0.00%");
-            //BarChartSeries barChartSeries3 = GenerateBarChartSeries("Defensive Strategy", 3U, 2U, pointNames, strategy, "0066CC", "0.00%");
-
-            AxisId axisId1 = new AxisId() { Val = (UInt32Value)92179456U };
-            AxisId axisId2 = new AxisId() { Val = (UInt32Value)92463872U };
-
-            barChart1.Append(barDirection1);
-            barChart1.Append(barGrouping1);
-            //barChart1.Append(barChartSeries1);
-            //barChart1.Append(barChartSeries2);
-            //barChart1.Append(barChartSeries3);
-            barChart1.Append(axisId1);
-            barChart1.Append(axisId2);
-
-            CategoryAxis categoryAxis1 = GenerateCategoryAxis(axisId1, AxisPositionValues.Bottom, "General", axisId2);
-
-            ValueAxis valueAxis1 = GenerateValueAxis(axisId2, AxisPositionValues.Left, "0%", axisId1);
-
-            ShapeProperties shapeProperties1 = new ShapeProperties();
-            A.NoFill noFill3 = new A.NoFill();
-
-            A.Outline outline8 = new A.Outline() { Width = 25400 };
-            A.NoFill noFill4 = new A.NoFill();
-
-            outline8.Append(noFill4);
-
-            shapeProperties1.Append(noFill3);
-            shapeProperties1.Append(outline8);
-
-            plotArea1.Append(layout2);
-            plotArea1.Append(barChart1);
-            plotArea1.Append(categoryAxis1);
-            plotArea1.Append(valueAxis1);
-            plotArea1.Append(shapeProperties1);
-
-            Legend legend1 = GenerateLegend(LegendPositionValues.Right);
-            PlotVisibleOnly plotVisibleOnly1 = new PlotVisibleOnly() { Val = true };
-            DisplayBlanksAs displayBlanksAs1 = new DisplayBlanksAs() { Val = DisplayBlanksAsValues.Gap };
-
-            chart1.Append(title1);
-            chart1.Append(plotArea1);
-            chart1.Append(legend1);
-            chart1.Append(plotVisibleOnly1);
-            chart1.Append(displayBlanksAs1);
-
-            return chart1;
         }
 
         protected override Layout GeneratePlotAreaLayout()
@@ -232,15 +162,8 @@ namespace RSMTenon.Graphing
             return categoryAxis1;
         }
 
-        public  Chart GenerateChartRise(string title)
+        public Chart GenerateChart(string title)
         {
-            //double[] global = { 0.7686D, 0.0555D };
-            //double[] bonds = { 0.1527D, 0.5255D };
-            //double[] strategy = { 0.2898D, 0.5510D };
-            //double[] current = { 0.3805D, 0.4397D };
-
-            //string[] pointNames = { "Bull Market Mar 03 - Mar 06", "10 Year Returns Dec 99 - Dec 09" };
-
             Chart chart1 = new Chart();
             Title title1 = GenerateTitle(title);
             PlotArea plotArea1 = new PlotArea();
@@ -251,20 +174,11 @@ namespace RSMTenon.Graphing
             BarDirection barDirection1 = new BarDirection() { Val = BarDirectionValues.Column };
             BarGrouping barGrouping1 = new BarGrouping() { Val = BarGroupingValues.Clustered };
 
-            //BarChartSeries barChartSeries1 = GenerateBarChartSeries("Global Equities", pointNames, global, "808080", "0.00%");
-            //BarChartSeries barChartSeries2 = GenerateBarChartSeries("UK Government Bonds", pointNames, bonds, "C0C0C0", "0.00%");
-            //BarChartSeries barChartSeries3 = GenerateBarChartSeries("Defensive Strategy", pointNames, strategy, "0066CC", "0.00%");
-            //BarChartSeries barChartSeries4 = GenerateBarChartSeries("Current Portfolio", pointNames, current, "99CC00", "0.00%");
-
             AxisId axisId1 = new AxisId() { Val = (UInt32Value)92179456U };
             AxisId axisId2 = new AxisId() { Val = (UInt32Value)92463872U };
 
             barChart1.Append(barDirection1);
             barChart1.Append(barGrouping1);
-            //barChart1.Append(barChartSeries4);
-            //barChart1.Append(barChartSeries1);
-            //barChart1.Append(barChartSeries2);
-            //barChart1.Append(barChartSeries3);
             barChart1.Append(axisId1);
             barChart1.Append(axisId2);
 
