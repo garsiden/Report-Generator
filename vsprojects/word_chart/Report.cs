201ac943b01eaa516148a69be3ccd7b733eeb5d1
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-19 17:32:01 +0000

    Add stress test market crash chart
    git-svn-id: svn://localhost/vantis/repgen/trunk@126 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/word_chart/Report.cs
+++ vsprojects/word_chart/Report.cs
@@ -26,7 +26,8 @@ namespace RSMTenon.ReportGenerator
         public DateTime FromDate;
         public DateTime ToDate;
         private string pointName;
-        private string format = "yyyy-mm-dd";
+        private string format = "yyyy-MM-dd";
+
         public StressTest(string id, XElement reportSpec)
         {
             var test1 = reportSpec.Element("categories").Elements().Single(t => t.Attribute("id").Value.Equals(id));
@@ -46,12 +47,12 @@ namespace RSMTenon.ReportGenerator
             get { return String.Format(pointName, FromDate.ToString("MMM yy"), ToDate.ToString("MMM yy")); }
             set { pointName = value; }
         }
-            
-        public int FromIntegerDate 
+
+        public int FromIntegerDate
         {
             get { return ReturnData.IntegerDate(FromDate); }
         }
-        public int ToIntegerDate 
+        public int ToIntegerDate
         {
             get { return ReturnData.IntegerDate(ToDate); }
         }
@@ -69,7 +70,7 @@ namespace RSMTenon.ReportGenerator
         // "C0C0C0", "808080", "0066CC", "98CC00"
         private string strategyColourHex = "0066CC";
         private string clientColourHex = "98CC00";
-        private DateTime tenYearStart = new DateTime(1999,9, 30);
+        private DateTime tenYearStart = new DateTime(1999, 9, 30);
 
         public ChartItem AllocationPieChart()
         {
@@ -116,16 +117,69 @@ namespace RSMTenon.ReportGenerator
             StressTestBarChart bc = new StressTestBarChart();
             C.Chart chart = bc.GenerateChartRise(title);
 
+            if (Client.ExistingAssets) {
+                var returns1 = getStressTestClientAssetReturn(Client.GUID);
+                var series1 = stressTestMarketRiseSeries(returns1, "Current", clientColourHex, rpt);
+                bc.AddBarChartSeries(chart, series1);
+            }
+
             // first asset class (Note: GLEQ)
-            var series2 = getAssetClassStressTestReturn(assetClasses[1], rpt);
+            var returns2 = getStressTestAssetClassReturn(assets[1].ID);
+            var series2 = stressTestMarketRiseSeries(returns2, assets[1].Name, assets[1].ColourHex, rpt);
             bc.AddBarChartSeries(chart, series2);
 
             // second asset class
-            var series3 = getAssetClassStressTestReturn(assetClasses[0], rpt);
+            var returns3 = getStressTestAssetClassReturn(assets[0].ID);
+            var series3 = stressTestMarketRiseSeries(returns3, assets[0].Name, assets[0].ColourHex, rpt);
             bc.AddBarChartSeries(chart, series3);
 
             // strategy
-            var series4 = getModelStressTestReturn(Client.StrategyID, rpt);
+            var returns4 = getStressTestModelReturn(Client.StrategyID);
+            var series4 = stressTestMarketRiseSeries(returns4, Client.Strategy.Name, strategyColourHex, rpt);
+            bc.AddBarChartSeries(chart, series4);
+
+            string ccn = rpt.Element("control-name").Value;
+            ChartItem chartItem = new ChartItem { Chart = chart, Title = title, CustomControlName = ccn };
+
+            return chartItem;
+
+        }
+
+        public ChartItem StressTestMarketCrash()
+        {
+            // get chart specs
+            XElement rpt = getChartSpec("stress-test-market-crash");
+
+            // set title
+            string title = rpt.Element("title").Value;
+
+            // get asset classes to plot (default is UKGB and GLEQ)
+            var assets = getAssetClasses();
+            var ctx = DataContext;
+
+            // create chart
+            StressTestBarChart bc = new StressTestBarChart();
+            C.Chart chart = bc.GenerateChartRise(title);
+
+            if (Client.ExistingAssets) {
+                var returns1 = getStressTestClientAssetReturn(Client.GUID);
+                var series1 = stressTestMarketCrashSeries(returns1, "Current", clientColourHex, rpt);
+                bc.AddBarChartSeries(chart, series1);
+            }
+
+            // first asset class (Note: GLEQ)
+            var returns2 = getStressTestAssetClassReturn(assets[1].ID);
+            var series2 = stressTestMarketCrashSeries(returns2, assets[1].Name, assets[1].ColourHex, rpt);
+            bc.AddBarChartSeries(chart, series2);
+
+            // second asset class
+            var returns3 = getStressTestAssetClassReturn(assets[0].ID);
+            var series3 = stressTestMarketCrashSeries(returns3, assets[0].Name, assets[0].ColourHex, rpt);
+            bc.AddBarChartSeries(chart, series3);
+
+            // strategy
+            var returns4 = getStressTestModelReturn(Client.StrategyID);
+            var series4 = stressTestMarketCrashSeries(returns4, Client.Strategy.Name, strategyColourHex, rpt);
             bc.AddBarChartSeries(chart, series4);
 
             string ccn = rpt.Element("control-name").Value;
@@ -152,17 +206,17 @@ namespace RSMTenon.ReportGenerator
             C.Chart chart = lc.GenerateChart(null);
 
             // first asset class
-            var data2 = getAssetClassTenYearReturn(assetClasses[0].ID, tenYearStart);
+            var data2 = tenYearAssetClassReturn(assetClasses[0].ID, tenYearStart);
             string dataKey2 = assetClasses[0].Name;
             lc.AddLineChartSeries(chart, data2.ToList(), dataKey2, assetClasses[0].ColourHex);
 
             // second asset class
-            var data3 = getAssetClassTenYearReturn(assetClasses[1].ID, tenYearStart);
+            var data3 = tenYearAssetClassReturn(assetClasses[1].ID, tenYearStart);
             string dataKey3 = assetClasses[1].Name;
             lc.AddLineChartSeries(chart, data3.ToList(), dataKey3, assetClasses[1].ColourHex);
 
             // strategy
-            var data4 = getModelTenYearReturn(Client.StrategyID, tenYearStart);
+            var data4 = tenYearModelReturn(Client.StrategyID, tenYearStart);
             string dataKey4 = StrategyName;
             lc.AddLineChartSeries(chart, data4, dataKey4, strategyColourHex);
 
@@ -173,11 +227,26 @@ namespace RSMTenon.ReportGenerator
 
         }
 
-        private BarGraphSeries getModelStressTestReturn(string strategyId, XElement rpt)
+        private Dictionary<int, ReturnData> getStressTestClientAssetReturn(Guid clientGuid)
         {
-            var test1 = new StressTest("bull", rpt);
+            var returns = DataContext.ClientAssetReturn(clientGuid);
+            var calc = new ReturnCalculation();
+            var prices = from p in returns
+                         select new ReturnData {
+                             Date = p.Date,
+                             Value = calc.calculatePrice(p)
+                         };
 
-            // Model prices
+            return prices.ToDictionary(p => p.Date);
+        }
+
+        private Dictionary<int, ReturnData> getStressTestAssetClassReturn(string assetClassId)
+        {
+            return DataContext.HistoricPrice(assetClassId).ToDictionary(d => d.Date);
+        }
+
+        private Dictionary<int, ReturnData> getStressTestModelReturn(string strategyId)
+        {
             var returns = DataContext.ModelReturn(strategyId);
             var calc = new ReturnCalculation();
             var prices = from p in returns
@@ -186,64 +255,63 @@ namespace RSMTenon.ReportGenerator
                              Value = calc.calculatePrice(p)
                          };
 
+            return prices.ToDictionary(p => p.Date);
+        }
+
+        private BarGraphSeries stressTestMarketCrashSeries(Dictionary<int, ReturnData> pd, string seriesName, string colourHex, XElement rpt)
+        {
+            var test1 = new StressTest("russian-debt-crisis", rpt);
+
+            // Russian Debt
+            var start1 = pd.TakeWhile(p => p.Key < test1.FromIntegerDate).Last();
+            var end1 = pd[test1.ToIntegerDate];
+            double return1 = (end1.Value - start1.Value.Value) / start1.Value.Value;
+
+            // Economic Slowdown
+            var test2 = new StressTest("economic-slowdown", rpt);
+            var start2 = pd[test2.FromIntegerDate];
+            var end2 = pd[test2.ToIntegerDate];
+            double return2 = (end2.Value - start2.Value) / start2.Value;
+
+            // Technology Bubble
+            var test3 = new StressTest("technology-bubble-burst", rpt);
+            var start3 = pd[test3.FromIntegerDate];
+            var end3 = pd[test3.ToIntegerDate];
+            double return3 = (end3.Value - start3.Value) / start3.Value;
+
+            var series = new BarGraphSeries {
+                Name = seriesName,
+                ColourHex = colourHex,
+                PointNames = new string[] { test1.PointName, test2.PointName, test3.PointName },
+                Values = new double[] { return1, return2, return3 }
+            };
+
+            return series;
+        }
+
+        private BarGraphSeries stressTestMarketRiseSeries(Dictionary<int, ReturnData> pd, string seriesName, string colourHex, XElement rpt)
+        {
+            var test1 = new StressTest("bull", rpt);
+
             // Bull
-            var pd = prices.ToDictionary(p => p.Date);
             var start1 = pd[test1.FromIntegerDate];
             var end1 = pd[test1.ToIntegerDate];
             double return1 = (end1.Value - start1.Value) / start1.Value;
 
             // Ten Year Return
             var test2 = new StressTest("ten-year", rpt);
-            var end2 = pd.Last();
             var start2 = pd.ElementAt(pd.Count() - 121);
+            var end2 = pd.Last();
+
             double return2 = Math.Log(end2.Value.Value / start2.Value.Value);
             test2.FromDate = start2.Value.DateFromInteger;
-            test2.ToDate = start2.Value.DateFromInteger;
-
-            var series = new BarGraphSeries {
-                Name = Client.Strategy.Name,
-                ColourHex = strategyColourHex,
-                PointNames = new string[] { test1.PointName, test2.PointName },
-                Values = new double[] {return1, return2 }
-            };
-            
-            return series;
-        }
-        private BarGraphSeries getAssetClassStressTestReturn(AssetClass assetClass, XElement rpt)
-        {
-            // series name
-            string seriesName = assetClass.Name;
-
-            // get historic prices
-            var hd = DataContext.HistoricPrice(assetClass.ID).ToDictionary(d => d.Date);
-            var last = hd.Last();
-
-            // Bull Run Mar-03 to Mar-06
-            var test1 = rpt.Element("categories").Elements().Single(t => t.Attribute("id").Value.Equals("bull"));
-            string pointName1 = test1.Element("point-name").Value;
-            string colourHex = assetClass.ColourHex;
-            var fromDT = DateTime.ParseExact(test1.Attribute("from").Value, "yyyy-mm-dd", new DateTimeFormatInfo());
-            var toDT = DateTime.ParseExact(test1.Attribute("to").Value, "yyyy-mm-dd", new DateTimeFormatInfo());
-
-            int fromInt = ReturnData.IntegerDate(fromDT);
-            int toInt = ReturnData.IntegerDate(toDT);
-            var start = hd[fromInt];
-            var end = hd[toInt];
-            double bull = (end.Value - start.Value) / start.Value;
-
-            // Ten Year Return
-            var test2 = rpt.Elements("categories").Elements().Single(t => t.Attribute("id").Value.Equals("ten-year"));
-            string pointName2 = test2.Element("point-name").Value;
-            var frm = hd.ElementAt(hd.Count() - 121);
-            var frmDT = frm.Value.DateFromInteger;
-            var lastDT = last.Value.DateFromInteger;
-            double ty = Math.Log(last.Value.Value / frm.Value.Value);
+            test2.ToDate = end2.Value.DateFromInteger;
 
             var series = new BarGraphSeries {
                 Name = seriesName,
-                ColourHex = assetClass.ColourHex,
-                PointNames = new string[] { String.Format(pointName1, fromDT.ToString("MMM yy"), toDT.ToString("MMM yy")),
-               String.Format(pointName2, frmDT.ToString("MMM yy"), lastDT.ToString("MMM yy"))}, Values = new double[] { bull, ty }
+                ColourHex = colourHex,
+                PointNames = new string[] { test1.PointName, test2.PointName },
+                Values = new double[] { return1, return2 }
             };
 
             return series;
@@ -251,16 +319,16 @@ namespace RSMTenon.ReportGenerator
 
 
 
-        private List<ReturnData> getModelTenYearReturn(string strategId, DateTime tenYearStart)
+        private List<ReturnData> tenYearModelReturn(string strategId, DateTime tenYearStart)
         {
             var data = DataContext.ModelReturn(strategId, tenYearStart);
 
             ReturnCalculation calc = new ReturnCalculation();
             var tyr = from d in data
-                        select new ReturnData {
-                            Value = calc.rebaseReturn(d),
-                            Date = d.Date
-                        };
+                      select new ReturnData {
+                          Value = calc.rebaseReturn(d),
+                          Date = d.Date
+                      };
 
             return tyr.ToList();
         }
@@ -292,7 +360,7 @@ namespace RSMTenon.ReportGenerator
 
             if (Client.ExistingAssets) {
                 string dataKey1 = "Current";
-                var data1 = ctx.ClientAssetPrice(Client.GUID);
+                var data1 = ctx.ClientAssetReturn(Client.GUID);
                 var rrex = getRollingReturn(data1, years);
                 lc.AddLineChartSeries(chart, rrex, dataKey1, clientColourHex);
             }
@@ -319,9 +387,8 @@ namespace RSMTenon.ReportGenerator
             return chartItem;
         }
 
-        private List<ReturnData> getAssetClassTenYearReturn(string assetClassId, DateTime startDate)
+        private List<ReturnData> tenYearAssetClassReturn(string assetClassId, DateTime startDate)
         {
-
             var data = DataContext.AssetClassReturn(startDate, assetClassId);
 
             ReturnCalculation calc = new ReturnCalculation();
@@ -333,7 +400,6 @@ namespace RSMTenon.ReportGenerator
 
             return tyr.ToList(); ;
         }
-
 
         private List<ReturnData> getRollingReturn(ISingleResult<ReturnData> data, int years)
         {
@@ -370,20 +436,20 @@ namespace RSMTenon.ReportGenerator
 
         private List<AssetClass> getAssetClasses(XElement rpt)
         {
-                var ctx = DataContext;
-                var allClasses = ctx.AssetClasses.ToDictionary(a => a.ID);
-                var assets = from spec in rpt.Descendants("asset-class")
-                             select spec;
+            var ctx = DataContext;
+            var allClasses = ctx.AssetClasses.ToDictionary(a => a.ID);
+            var assets = from spec in rpt.Descendants("asset-class")
+                         select spec;
 
-                var classes = new List<AssetClass>();
-                foreach (var a in assets) {
-                    classes.Add(new AssetClass() {
-                        ID = a.Attribute("id").Value,
-                        Name = allClasses[a.Attribute("id").Value].Name,
-                        ColourHex = a.Attribute("colour-hex").Value
-                    });
-                }
-                assetClasses = classes;
+            var classes = new List<AssetClass>();
+            foreach (var a in assets) {
+                classes.Add(new AssetClass() {
+                    ID = a.Attribute("id").Value,
+                    Name = allClasses[a.Attribute("id").Value].Name,
+                    ColourHex = a.Attribute("colour-hex").Value
+                });
+            }
+            assetClasses = classes;
 
             return assetClasses;
         }
@@ -412,7 +478,7 @@ namespace RSMTenon.ReportGenerator
 
         private XElement getChartSpec(string id)
         {
-            var spec = (from rep in ChartSpecs.Descendants("report")
+            var spec = (from rep in ChartSpecs.Descendants("chart")
                         where rep.Attribute("id").Value == id
                         select rep).Single();
 
