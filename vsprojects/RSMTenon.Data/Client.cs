38d55092c821e76792e03a1cc9ed9c2df256c0ed
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-27 15:21:30 +0000

    Add convenience properties to Client class
    git-svn-id: svn://localhost/vantis/repgen/trunk@155 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/RSMTenon.Data/Client.cs
+++ vsprojects/RSMTenon.Data/Client.cs
@@ -8,11 +8,33 @@ namespace RSMTenon.Data
     public partial class Client
     {
 
+        //private Strategy strategy;
+
         public static Client GetClientByGUID(Guid clientGuid)
         {
             var ctx = new RepGenDataContext();
 
             return ctx.Clients.Single(c => c.GUID == clientGuid);
+        }
+
+        public string ReportingFrequency
+        {
+            get
+            {
+                if (this.InvestmentAmount >= 1000000) {
+                    return "quarterly";
+                } else {
+                    return "half-yearly";
+                }
+            }
+        }
+
+        public decimal InitialFeeAmount
+        {
+            get
+            {
+                return this.InitialFee * this.InvestmentAmount;
+            }
         }
     }
 }
