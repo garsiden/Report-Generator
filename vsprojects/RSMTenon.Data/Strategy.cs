91c1d1e3f7bd36446fb79b8f09183d4003b940b6
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-27 17:37:41 +0000

    Add instance method to retrieve model return
    git-svn-id: svn://localhost/vantis/repgen/trunk@158 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/RSMTenon.Data/Strategy.cs
+++ vsprojects/RSMTenon.Data/Strategy.cs
@@ -7,12 +7,32 @@ namespace RSMTenon.Data
 {
     public partial class Strategy
     {
+        private Dictionary<int, ReturnData> strategyReturn;
+
         public static string GetStrategyNameFromId(string id)
         {
             var ctx = new RepGenDataContext();
 
             return ctx.Strategies.First(s => s.ID.Equals(id)).Name;
 
-        } 
+        }
+
+        public Dictionary<int, ReturnData> GetStrategyReturn()
+        {
+            if (strategyReturn == null) {
+                var ctx = new RepGenDataContext();
+                var returns = ctx.ModelReturn(this.ID);
+                var calc = new ReturnCalculation();
+                var prices = from p in returns
+                             select new ReturnData {
+                                 Date = p.Date,
+                                 Value = calc.Price(p)
+                             };
+
+                strategyReturn = prices.ToDictionary(p => p.Date);
+            }
+
+            return strategyReturn;
+        }
     }
 }
