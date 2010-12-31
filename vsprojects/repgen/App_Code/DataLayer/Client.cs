b43436ca760d74e58ab898ec3728a616594038ac
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2010-12-31 15:27:00 +0000

    Add DataObject methods for use with ObjectDataSource
    git-svn-id: svn://localhost/vantis/repgen/trunk@177 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/App_Code/DataLayer/Client.cs
+++ vsprojects/repgen/App_Code/DataLayer/Client.cs
@@ -5,18 +5,74 @@ using System.Text;
 
 namespace RSMTenon.Data
 {
-    public partial class Client
+    [System.ComponentModel.DataObject]
+    public partial class Client : Data.RepGenDataContext
     {
-
-        //private Strategy strategy;
-
-        public static Client GetClientByGUID(Guid clientGuid)
+        #region DataObjectMethods
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, false)]
+        public static Client GetClientByGUID(Guid? clientGuid)
         {
             var ctx = new RepGenDataContext();
-
-            return ctx.Clients.Single(c => c.GUID == clientGuid);
+            if (clientGuid == null) {
+                return null;
+            } else {
+                return ctx.Clients.SingleOrDefault(c => c.GUID == clientGuid);
+            }
         }
 
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, false)]
+        public static List<Client> GetRecentClients(int number)
+        {
+            var ctx = new RepGenDataContext();
+            return ctx.Clients.OrderByDescending(c => c.MeetingDate).Take(number).ToList();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Update, true)]
+        public void UpdateClient(Client client)
+        {
+            var ctx = new RepGenDataContext();
+            ctx.Clients.Attach(client, true);
+            ctx.SubmitChanges();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Update, false)]
+        public static void UpdateClient(Client client, Client original_client)
+        {
+            var ctx = new RepGenDataContext();
+            ctx.Clients.Attach(client, original_client);
+            ctx.SubmitChanges();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Insert, true)]
+        public static Guid InsertClient(Client client)
+        {
+            var ctx = new RepGenDataContext();
+            ctx.Clients.InsertOnSubmit(client);
+            ctx.SubmitChanges();
+
+            return client.GUID;
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Delete, false)]
+        public static void DeleteClient(Guid guid)
+        {
+            var ctx = new RepGenDataContext();
+            ctx.Clients.DeleteOnSubmit(ctx.Clients.Single(c => c.GUID == guid));
+            ctx.SubmitChanges();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Delete, true)]
+        public static void DeleteClient(Client client)
+        {
+            var ctx = new RepGenDataContext();
+            ctx.Clients.Attach(client);
+            ctx.Clients.DeleteOnSubmit(client);
+            ctx.SubmitChanges();
+        }
+
+        #endregion DataObjectMethods
+
+        #region Extended Properties
         public string ReportingFrequency
         {
             get
@@ -37,25 +93,6 @@ namespace RSMTenon.Data
             }
         }
 
-        public static List<Client> GetRecentClients(int number)
-        {
-            var ctx = new RepGenDataContext();
-
-            return ctx.Clients.OrderByDescending(c => c.MeetingDate).Take(number).ToList();
-
-            //var clients = ctx.Clients;
-
-            //var recent = from c in clients
-            //             orderby c.MeetingDate descending
-            //             select new
-            //             {
-            //                 GUID = c.GUID,
-            //                 Name = c.Name,
-            //                 MeetingDate = c.MeetingDate
-            //             };
-
-            //return recent.Take(number).ToList();
-                            
-        }
+        #endregion
     }
 }
