2b213c17f73caca5ff6d03b50c859a29f00a67fd
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-03 19:22:19 +0000

    Move report download code to base class
    git-svn-id: svn://localhost/vantis/repgen/trunk@212 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/App_Code/RepGenPage.cs
+++ vsprojects/repgen/App_Code/RepGenPage.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
 using RSMTenon.Data;
+using RSMTenon.ReportGenerator;
 
 public partial class RepGenPage : System.Web.UI.Page
 
@@ -33,4 +35,28 @@ public partial class RepGenPage : System.Web.UI.Page
         }
     }
 
+    protected void DownloadReport(Guid clientGuid)
+    {
+        Client client = Client.GetClientByGUID(clientGuid);
+        Report report = new Report() { Client = client };
+        var repgen = new ReportGenerator();
+        string tempDocName = repgen.CreateReport(report);
+
+        try
+        {
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Response.AppendHeader("Content-Disposition", "attachment; filename=repgen.dotx");
+            Response.AppendHeader("Content-Type", "application/msword");
+            Response.ContentEncoding = System.Text.Encoding.UTF8;
+            Response.TransmitFile(tempDocName);
+            Response.Flush();
+            Response.Close();
+        }
+        finally
+        {
+            if (File.Exists(tempDocName))
+                File.Delete(tempDocName);
+        }
+    }
 }
--- vsprojects/repgen/Pages/Client/edit.aspx.cs
+++ vsprojects/repgen/Pages/Client/edit.aspx.cs
@@ -10,8 +10,6 @@ using RSMTenon.ReportGenerator;
 
 public partial class Pages_Client_edit : RepGenPage
 {
-    private RepGenDataContext context;
-
     protected void Page_Load(object sender, EventArgs e)
     {
         ExceptionDetails.Visible = false;
@@ -37,28 +35,7 @@ public partial class Pages_Client_edit : RepGenPage
 
     protected void btnCreateReport_Click(object sender, EventArgs e)
     {
-
         Guid clientGuid = new Guid(this.Request.QueryString["guid"]);
-        Client client = Client.GetClientByGUID(clientGuid);
-        Report report = new Report() { Client = client };
-        var repgen = new ReportGenerator();
-        string tempDocName = repgen.CreateReport(report);
-
-        try
-        {
-            Response.ClearContent();
-            Response.ClearHeaders();
-            Response.AppendHeader("Content-Disposition", "attachment; filename=repgen.dotx");
-            Response.AppendHeader("Content-Type", "application/msword");
-            Response.ContentEncoding = System.Text.Encoding.UTF8;
-            Response.TransmitFile(tempDocName);
-            Response.Flush();
-            Response.Close();
-        }
-        finally
-        {
-            if (File.Exists(tempDocName))
-                File.Delete(tempDocName);
-        }
+        DownloadReport(clientGuid);
     }
 }
