0a2fc1704cad15e297de950ca0e31431f9f03503
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-03 19:07:19 +0000

    Base class for all pages
    git-svn-id: svn://localhost/vantis/repgen/trunk@200 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/App_Code/RepGenPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RSMTenon.Data;
+
+public partial class RepGenPage : System.Web.UI.Page
+
+{
+    protected RepGenDataContext context;
+
+    public IEnumerable<Strategy> GetStrategies()
+    {
+        return DataContext.Strategies;
+    }
+
+    public IEnumerable<Benchmark> GetBenchmarks()
+    {
+        return DataContext.Benchmarks.OrderBy(b => b.Name);
+    }
+
+    protected RepGenDataContext DataContext
+    {
+        get
+        {
+            if (context == null)
+            {
+                context = new RepGenDataContext();
+            }
+            return context;
+        }
+    }
+
+}
