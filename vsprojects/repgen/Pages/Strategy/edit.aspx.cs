a2456842b2655b1b4374b6448688c01989e7a33c
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-03 14:13:49 +0000

    Add strategy pages
    git-svn-id: svn://localhost/vantis/repgen/trunk@195 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/Pages/Strategy/edit.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RSMTenon.Data;
+
+public partial class Pages_Strategy_edit : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    public IEnumerable<Benchmark> GetBenchmarks()
+    {
+        var ctx = new RepGenDataContext();
+
+        return ctx.Benchmarks.OrderBy(b => b.Name);
+    }
+
+}
