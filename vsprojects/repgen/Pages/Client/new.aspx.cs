05e066112e21a379e8d2d2c44ed9b0f67b7d32de
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-03 19:11:27 +0000

    Remove old report generation test code; now in ReportGenerator class
    git-svn-id: svn://localhost/vantis/repgen/trunk@205 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/Pages/Client/new.aspx.cs
+++ vsprojects/repgen/Pages/Client/new.aspx.cs
@@ -7,86 +7,11 @@ using System.Web.UI.WebControls;
 using DocumentFormat.OpenXml.Packaging;
 using System.IO;
 using System.Xml;
-using RSMTenon.Data;
 
-public partial class Pages_Client_new  : System.Web.UI.Page
+public partial class Pages_Client_new  : RepGenPage
 {
-    protected RepGenDataContext context;
-
     protected void Page_Load(object sender, EventArgs e)
     {
 
-    }
-
-    protected RepGenDataContext getContext()
-    {
-        if (context == null)
-            context = new RepGenDataContext();
-
-        return context;
-    }
-
-    public IEnumerable<Strategy> GetStrategies()
-    {
-        return getContext().Strategies;
-    }
-
-    protected void createReportButton_Click(object sender, EventArgs e)
-    {
-        string strTemp = Environment.GetEnvironmentVariable("temp");
-        string strFileName = String.Format("{0}\\{1}.dotx", strTemp, Guid.NewGuid().ToString());
-        File.Copy(Server.MapPath(@"App_Data/suitable1.dotx"), strFileName);
-
-        GetContent();
-        string customXml = File.ReadAllText(Server.MapPath(@"App_Data/content_temp.xml"));
-        ReplaceCustomXML(strFileName, customXml);
-
-        //return it to the client - we know strFile is updated, so return it
-        Response.ClearContent();
-        Response.ClearHeaders();
-        Response.AppendHeader("Content-Disposition", "attachment; filename=ClientSuitable.dotx");
-        Response.AppendHeader("Content-Type", "application/msword");
-        Response.ContentEncoding = System.Text.Encoding.UTF8;
-        Response.TransmitFile(strFileName);
-        Response.Flush();
-        Response.Close();
-
-        //Delete the temp file
-        File.Delete(strFileName);
-        File.Delete(Server.MapPath(@"App_Data/content_temp.xml"));
-    }
-
-    protected void GetContent()
-    {
-
-        // create temp content file
-        File.Copy(Server.MapPath(@"App_Data/content.xml"), Server.MapPath(@"App_Data/content_temp.xml"), true);
-
-        
-
-        //doc.Save(Server.MapPath(@"App_Data/content_temp.xml"));
-
-    }
-
-    protected void ReplaceCustomXML(string fileName, string customXML)
-    {
-        using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(fileName, true))
-        {
-            MainDocumentPart mainPart = wordDoc.MainDocumentPart;
-
-            mainPart.DeleteParts<CustomXmlPart>(mainPart.CustomXmlParts);
-
-            //Add a new customXML part and then add content
-            CustomXmlPart customXmlPart = mainPart.AddCustomXmlPart(CustomXmlPartType.CustomXml);
-
-            //copy the XML into the new part...
-            using (StreamWriter ts = new StreamWriter(customXmlPart.GetStream()))
-                ts.Write(customXML);
-        }
-    }
-    protected void sourceClient_Inserting(object sender, LinqDataSourceInsertEventArgs e)
-    {
-        Client client = (Client)e.NewObject;
-        client.GUID = Guid.NewGuid();
     }
 }
