0378c838e162e61ad0b0e4075fd21c79f6c9ac8e
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-05 20:41:04 +0000

    Upload to database with bulk insert
    git-svn-id: svn://localhost/vantis/repgen/trunk@231 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/App_Code/DataLayer/Benchmark.cs
+++ vsprojects/repgen/App_Code/DataLayer/Benchmark.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace RSMTenon.Data
 {
     [System.ComponentModel.DataObject]
     public partial class Benchmark
     {
+        public static void UploadToDatabase(DataUpload.BenchmarkDataTable dt)
+        {
+            string tableName = "tblBenchmarkDataTest";
+            string sql = String.Format("DELETE FROM {0}", tableName);
+
+            using (SqlConnection cn = ConnectionFactory.CreateSqlConnection())
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand(sql, cn);
+                int deleted = cmd.ExecuteNonQuery();
+                Console.WriteLine("{0} record(s) deleted", deleted);
+
+                using (SqlBulkCopy bc = new SqlBulkCopy(cn))
+                {
+                    bc.DestinationTableName = tableName;
+                    bc.WriteToServer(dt);
+                }
+            }
+        }
 
     }
+
 }
