1b7c76d3036297015b50157af282b69f9dac66f5
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-06 17:52:50 +0000

    New page to select template file
    git-svn-id: svn://localhost/vantis/repgen/trunk@234 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/Pages/Template/index.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/SiteMasterPage.master" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="Pages_Template_index" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label ID="Label1" runat="server" Text="Label">Select template:&nbsp;</asp:Label>
+<asp:DropDownList ID="dropList" runat="server" 
+    DataSource="<%# GetTemplates() %>" DataTextField="Name" DataValueField="Name" 
+    Width="165px">
+
+    </asp:DropDownList>
+<br /><br />
+
+    <asp:Button ID="btnSetTemplate" runat="server" Text="Set Template" 
+        onclick="btnSetTemplate_Click" />
+</asp:Content>
+
---
+++ vsprojects/repgen/Pages/Template/index.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Configuration;
+using System.Web.Configuration;
+
+public partial class Pages_Template_index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            this.DataBind();
+            this.dropList.SelectedValue = GetSelectedTemplate();
+        }
+    }
+
+    public FileInfo[] GetTemplates()
+    {
+        string templateDir = Server.MapPath("~/App_Data/templates");
+        DirectoryInfo info = new DirectoryInfo(templateDir);
+        FileInfo[] templates = info.GetFiles("*.dotx");
+
+        return templates;
+
+    }
+
+    public string GetSelectedTemplate()
+    {
+        string path = Server.MapPath(Request.CurrentExecutionFilePath);
+        Configuration config = WebConfigurationManager.OpenWebConfiguration("~/");
+
+        string template = config.AppSettings.Settings["TemplateFile"].Value;
+
+        return template;
+
+    }
+    protected void btnSetTemplate_Click(object sender, EventArgs e)
+    {
+        Configuration config = WebConfigurationManager.OpenWebConfiguration("~/");
+
+        string current = GetSelectedTemplate();
+        string selected = this.dropList.SelectedValue.ToString();
+        if (current != selected)
+        {
+            config.AppSettings.Settings["TemplateFile"].Value = selected;
+            config.Save();
+        }
+    }
+}
