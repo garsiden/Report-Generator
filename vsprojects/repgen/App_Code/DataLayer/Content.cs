5e2aa53461cd4c7e5bac71a8277090298ecfdeab
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-09 16:39:19 +0000

    Add methodsfor ODS
    git-svn-id: svn://localhost/vantis/repgen/trunk@257 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/App_Code/DataLayer/Content.cs
+++ vsprojects/repgen/App_Code/DataLayer/Content.cs
@@ -8,5 +8,47 @@ namespace RSMTenon.Data
     [System.ComponentModel.DataObject]
     public partial class Content
     {
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, false)]
+        public static IQueryable<Content> GetContent(string strategyId)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+
+            var predicate = PredicateBuilder.Make<RSMTenon.Data.Content>();
+
+            switch (strategyId)
+            {
+                case "":
+                    predicate = PredicateBuilder.Make<RSMTenon.Data.Content>(c => c.StrategyID == null);
+                    break;
+                case "%":
+                    predicate = PredicateBuilder.Make<RSMTenon.Data.Content>(c => c.StrategyID != null);
+                    break;
+                default:
+                    predicate = PredicateBuilder.Make<RSMTenon.Data.Content>(c => c.StrategyID == strategyId);
+                    break;
+            }
+
+            //var predicate = PredicateBuilder.Make<Customer>(c => c.Name.Contains("A"));
+            //predicate = predicate.Or(c => c.Name.Contains("B"));
+            //predicate = predicate.Or(c => c.Name.Contains("C"));
+            //predicate = predicate.Or(c => c.Name.Contains("X") && c.Age > 30);
+            //predicate = predicate.Or(c => c.Age > 50 && c.Age < 55);
+
+            var contents = ctx.Contents;
+
+            var match = contents.Where(predicate);
+
+            return match;
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Update, true)]
+        public static void UpdateClient(Content content, Content original_content)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            ctx.Contents.Attach(content, original_content);
+            ctx.SubmitChanges();
+        }
+
+
     }
 }
