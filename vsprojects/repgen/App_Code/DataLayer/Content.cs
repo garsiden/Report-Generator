d92c98f1066609c078e511c1b0184770331ee8b3
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-09 19:24:12 +0000

    Try various select methods for ODS
    git-svn-id: svn://localhost/vantis/repgen/trunk@259 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/App_Code/DataLayer/Content.cs
+++ vsprojects/repgen/App_Code/DataLayer/Content.cs
@@ -2,12 +2,102 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
 
 namespace RSMTenon.Data
 {
     [System.ComponentModel.DataObject]
     public partial class Content
     {
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, false)]
+        public static IQueryable<Content> GetContent(string strategyId, int contentIdx, int categoryIdx)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            var predicate = PredicateBuilder.Make<RSMTenon.Data.Content>();
+
+            switch (contentIdx)
+            {
+                case 0:
+                    predicate = predicate.And(c => c.ContentID.StartsWith("strategy."));
+                    break;
+                case 1:
+                    predicate = predicate.And(c => c.ContentID.StartsWith("charts."));
+                    break;
+                case 2:
+                    predicate = predicate.Or(c => c.ContentID.Length > 0);
+                    break;
+            }
+
+            switch (categoryIdx)
+            {
+                case 0:
+                    predicate = predicate.And(c => c.Category == "existing-assets");
+                    break;
+                case 1:
+                    predicate = predicate.And(c => c.Category == "no-existing-assets");
+                    break;
+            }
+
+            switch (strategyId)
+            {
+                case "%":
+                    //predicate = predicate.And(c => c.StrategyID != null);
+                    break;
+                case "":
+                    predicate = predicate.And(c => c.StrategyID == null);
+                    break;                  
+                default:
+                    predicate = predicate.And(c => c.StrategyID == strategyId);
+                    break;
+            }
+
+            var contents = ctx.Contents;
+
+            var match = contents.Where(predicate).OrderBy(c => c.ContentID).ThenBy(c => c.Category);
+
+            return match;
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, false)]
+        public static IQueryable<Content> GetContent(string strategyId, bool strategy, bool charts, bool cash, bool assets, bool general)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+
+            var predicate = PredicateBuilder.Make<RSMTenon.Data.Content>();
+
+
+            if (!charts)
+                predicate = predicate.And(c => !c.ContentID.StartsWith("charts."));
+            else
+                predicate = predicate.Or(c => c.ContentID.StartsWith("charts."));
+
+            //if (!cash)
+            //    predicate = predicate.Or(c => !c.Category.Equals("no-existing-assets"));
+            //if (!assets)
+            //    predicate = predicate.Or(c => !c.Category.Equals("existing-assets"));
+            //if (!general)
+            //    predicate = predicate.Or(c => c.StrategyID != null);
+            if (strategy)
+                switch (strategyId)
+                {
+                    case "%":
+                        predicate = predicate.And(c => c.StrategyID != null);
+                        break;
+                    default:
+                        predicate = predicate.And(c => c.StrategyID == strategyId);
+                        break;
+                } else
+                predicate = predicate.And(c => c.StrategyID == null);
+
+
+
+            var contents = ctx.Contents;
+
+            var match = contents.Where(predicate).OrderBy(c => c.ContentID).ThenBy(c => c.Category);
+
+            return match;
+        }
+
         [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, false)]
         public static IQueryable<Content> GetContent(string strategyId)
         {
@@ -36,7 +126,7 @@ namespace RSMTenon.Data
 
             var contents = ctx.Contents;
 
-            var match = contents.Where(predicate);
+            var match = contents.Where(predicate).OrderBy(c => c.ContentID).ThenBy(c => c.Category);
 
             return match;
         }
