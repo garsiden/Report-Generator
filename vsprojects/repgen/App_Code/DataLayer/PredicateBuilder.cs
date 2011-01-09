5a350dc2b2c227e8a2744cacf3099790747d1625
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-09 16:38:47 +0000

    Class to allow building of query criteria
    git-svn-id: svn://localhost/vantis/repgen/trunk@256 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/App_Code/DataLayer/PredicateBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+
+namespace RSMTenon.Data
+{
+    public static class PredicateBuilder
+    {
+        public static Expression<Func<T, bool>> Make<T>() { return null; }
+
+        public static Expression<Func<T, bool>> Make<T>(this Expression<Func<T, bool>> predicate)
+        {
+            return predicate;
+        }
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr, Expression<Func<T, bool>> or)
+        {
+            if (expr == null)
+                return or;
+            var invokedExpr = Expression.Invoke(or, expr.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>(Expression.Or(expr.Body, invokedExpr), expr.Parameters);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr, Expression<Func<T, bool>> and)
+        {
+            if (expr == null)
+                return and;
+            var invokedExpr = Expression.Invoke(and, expr.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>(Expression.And(expr.Body, invokedExpr), expr.Parameters);
+        }
+    }
+
+}
