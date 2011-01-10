2b6f3f6992ffd2caee82b22936c042739a5eeda8
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-10 20:54:16 +0000

    Add ODS methods to class
    git-svn-id: svn://localhost/vantis/repgen/trunk@263 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/App_Code/DataLayer/ClientAsset.cs
+++ vsprojects/repgen/App_Code/DataLayer/ClientAsset.cs
@@ -8,6 +8,57 @@ namespace RSMTenon.Data
     [System.ComponentModel.DataObject]
     public partial class ClientAsset
     {
+        #region DataObjectMethods
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, true)]
+        public static ClientAsset GetClientAsset(Guid guid)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            return ctx.ClientAssets.SingleOrDefault(a => a.GUID == guid);
+
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, true)]
+        public static IQueryable<ClientAsset> GetAllClientsAssets(Guid clientGuid)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            return ctx.ClientAssets.Where(a => a.ClientGUID == clientGuid);
+
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Update, true)]
+        public static void UpdateClientAsset(ClientAsset clientAsset, ClientAsset original_clientAsset)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            ctx.ClientAssets.Attach(clientAsset, original_clientAsset);
+            ctx.SubmitChanges();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Delete, true)]
+        public static void DeleteClientAsset(ClientAsset clientAsset)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            ctx.ClientAssets.DeleteOnSubmit(ctx.ClientAssets.Single(c => c.GUID == clientAsset.GUID));
+            ctx.SubmitChanges();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Insert, true)]
+        public static Guid InsertClient(ClientAsset clientAsset)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            ctx.ClientAssets.InsertOnSubmit(clientAsset);
+            ctx.SubmitChanges();
+
+            return clientAsset.GUID;
+        }
+
+        #endregion
+        public decimal TotalAssetAllocation
+        {
+            get
+            {
+                return CASH + COMM + COPR + GLEQ + HEDG + + LOSH + PREQ + UKCB + UKEQ + UKGB + UKHY + WOBO;
+            }
+        }
 
     }
 }
