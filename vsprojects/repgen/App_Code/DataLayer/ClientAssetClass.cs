95aca9a6f9e8234ca5cf1d5b669f5071ae4bd41f
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-11 22:11:29 +0000

    Add ODS methods to Client Asset class
    git-svn-id: svn://localhost/vantis/repgen/trunk@268 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/App_Code/DataLayer/ClientAssetClass.cs
+++ vsprojects/repgen/App_Code/DataLayer/ClientAssetClass.cs
@@ -8,17 +8,68 @@ namespace RSMTenon.Data
     [System.ComponentModel.DataObject]
     public partial class ClientAssetClass
     {
-        public static IQueryable<AssetWeighting> GetClientAssetClass(Guid clientGuid)
+        #region DataObjectMethods
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, true)]
+        public static ClientAssetClass GetClientAssetClass(Guid clientGuid)
         {
             var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
-            var assets = ctx.ClientAssetClasses;
+            return ctx.ClientAssetClasses.SingleOrDefault(a => a.ClientGUID == clientGuid);
 
-            var weighting = from asset in assets
-                            where asset.ClientGUID.Equals(clientGuid)
-                            //group asset by asset.AssetClass.Name  into g
-                            //select new AssetWeighting { AssetClass = g.Key, Allocation = g.Sum(asset => asset.Weighting) };
-                            select new AssetWeighting { AssetClass = asset.AssetClass.Name, Weighting = asset.Weighting };
-            return weighting;
         }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Select, false)]
+        public static IQueryable<ClientAssetClass> GetAllClientsAssets()
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            return ctx.ClientAssetClasses;
+
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Update, true)]
+        public static void UpdateClientAsset(ClientAssetClass clientAsset, ClientAssetClass original_clientAsset)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            ctx.ClientAssetClasses.Attach(clientAsset, original_clientAsset);
+            ctx.SubmitChanges();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Delete, true)]
+        public static void DeleteClientAsset(ClientAssetClass clientAsset)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            ctx.ClientAssetClasses.DeleteOnSubmit(ctx.ClientAssetClasses.Single(c => c.ClientGUID == clientAsset.ClientGUID));
+            ctx.SubmitChanges();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Delete, false)]
+        public static void DeleteClientAsset(Guid clientGuid)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            ctx.ClientAssetClasses.DeleteOnSubmit(ctx.ClientAssetClasses.Single(c => c.ClientGUID == clientGuid));
+            ctx.SubmitChanges();
+        }
+
+        [System.ComponentModel.DataObjectMethod(System.ComponentModel.DataObjectMethodType.Insert, true)]
+        public void InsertClientAsset(ClientAssetClass clientAsset)
+        {
+            var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+            ctx.ClientAssetClasses.InsertOnSubmit(clientAsset);
+            ctx.SubmitChanges();
+        }
+
+        #endregion
+
+        //public static IQueryable<AssetWeighting> GetClientAssetClass(Guid clientGuid)
+        //{
+        //    var ctx = new RepGenDataContext(ConnectionFactory.CreateSqlConnection());
+        //    var assets = ctx.ClientAssetClasses;
+
+        //    var weighting = from asset in assets
+        //                    where asset.ClientGUID.Equals(clientGuid)
+        //                    //group asset by asset.AssetClass.Name  into g
+        //                    //select new AssetWeighting { AssetClass = g.Key, Allocation = g.Sum(asset => asset.Weighting) };
+        //                    select new AssetWeighting { AssetClass = asset.AssetClass.Name, Weighting = asset.Weighting };
+        //    return weighting;
+        //}
     }
 }
