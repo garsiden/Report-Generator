be7cc82fbdb05b17e1c42dcfc77df0609a16d63d
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-13 11:31:39 +0000

    Reinstate ConnectionFactory - used for upload of CSV files to database
    git-svn-id: svn://localhost/vantis/repgen/trunk@294 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/App_Code/DataLayer/ConnectionFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace RSMTenon.Data
+{
+
+    /// <summary>
+    /// Summary description for ConnectionFactory
+    /// </summary>
+    public class ConnectionFactory
+    {
+        static public SqlConnection CreateSqlConnection()
+        {
+
+            string appName = System.Web.HttpContext.Current.Request.ApplicationPath.Substring(1);
+            string dbName;
+
+            dbName = ConfigurationSettings.AppSettings["DbDefault"];
+
+            return CreateSqlConnection(dbName);
+        }
+
+        static public SqlConnection CreateSqlConnection(string dbName)
+        {
+
+            NameValueCollection appSettings = (NameValueCollection)ConfigurationManager.GetSection("appSettings");
+            string server = appSettings["DbServer"];
+            string user = appSettings["DbUser"];
+            string pwd = appSettings["DbPwd"];
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "SERVER=" + server +
+                                    ";DATABASE=" + dbName +
+                                    ";UID=" + user +
+                                    ";PWD=" + pwd;
+            return conn;
+        }
+    }
+}
