86756e5dfad2ea0f880985824783a3aacae9acb4
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-16 12:19:11 +0000

    Add data input error handling and investment summary
    git-svn-id: svn://localhost/vantis/repgen/trunk@319 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/Pages/Client/assets.aspx.cs
+++ vsprojects/repgen/Pages/Client/assets.aspx.cs
@@ -10,6 +10,9 @@ using RSMTenon.Data;
 
 public partial class Pages_Client_assets : RepGenPage
 {
+    private decimal totalAssets = 0;
+    private Client client;
+
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
@@ -18,10 +21,15 @@ public partial class Pages_Client_assets : RepGenPage
             if (guidString != null)
             {
                 Guid guid = new Guid(guidString);
-                Client client = Client.GetClientByGUID(guid);
-                gridAsset.Caption = client.Name;
+                client = Client.GetClientByGUID(guid);
+                //gridAsset.Caption = client.Name;
+                clientAssetHeader.InnerText = "Client Assets By Class for " + client.Name;
             }
+
+            gridAsset.DataBind();
         }
+
+        labelExceptionDetails.Visible = labelException.Visible = false;
     }
 
     protected void detailsView_DataBound(object sender, EventArgs e)
@@ -32,17 +40,19 @@ public partial class Pages_Client_assets : RepGenPage
         {
             string name = asset.AssetName;
             this.detailsView.Caption = name;
-            string text = String.Format("Total Asset Allocation: {0:0.0%}", asset.TotalAssetAllocation);
-            this.labelAssetAllocation.Text = text;
+        } else
+        {
+            detailsView.Caption = null;
         }
     }
+
     protected void gridAsset_RowCommand(object sender, GridViewCommandEventArgs e)
     {
         if (e.CommandName == "Insert")
         {
             TextBox textName = null;
             TextBox textAmount = null;
-            
+
             string guidString = Request.QueryString["guid"].ToString();
             Guid clientGuid = new Guid(guidString);
             var source = e.CommandSource;
@@ -50,16 +60,20 @@ public partial class Pages_Client_assets : RepGenPage
             if (e.CommandArgument == "AllNew")
             {
                 Table tbl = (Table)gridAsset.Controls[0];
+                //var tbl2 = gridAsset.Controls[1];
                 GridViewRow gvr = (GridViewRow)tbl.Controls[0];
-                textName = (TextBox)gvr.FindControl("textAssetNameAdd");
-                textAmount = (TextBox)gvr.FindControl("textAmountAdd");
-            } else {
+                textName = (TextBox)gvr.FindControl("textAssetNameAddAllNew");
+                textAmount = (TextBox)gvr.FindControl("textAmountAddAllNew");
+            } else
+            {
                 textName = (TextBox)gridAsset.FooterRow.FindControl("textAssetNameAdd");
                 textAmount = (TextBox)gridAsset.FooterRow.FindControl("textAmountAdd");
             }
-            
+
             string name = textName.Text;
-            decimal amount = Convert.ToDecimal(textAmount.Text);
+            decimal amount = 0;
+            if (textAmount.Text != String.Empty)
+                amount = Convert.ToDecimal(textAmount.Text);
 
             ClientAsset asset = new ClientAsset()
             {
@@ -72,6 +86,102 @@ public partial class Pages_Client_assets : RepGenPage
             int nrows = gridAsset.Rows.Count;
             if (nrows >= 1)
                 gridAsset.SelectedIndex = nrows - 1;
+        } else if (e.CommandName == "Delete")
+        {
+            gridAsset.DataBind();
+            int nrows = gridAsset.Rows.Count;
+            if (nrows > 1)
+                gridAsset.SelectedIndex = 0;
+            else
+                gridAsset.SelectedIndex = -1;
+        }
+
+    }
+    protected void detailsView_ItemUpdated(object sender, DetailsViewUpdatedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            labelExceptionDetails.Visible = true;
+            labelExceptionDetails.Text = "There was a problem updating the client assets. ";
+            labelExceptionDetails.Text += "<br/>";
+            labelExceptionDetails.Text += e.Exception.InnerException.Message;
+            e.ExceptionHandled = true;
+            e.KeepInEditMode = true;
+        }
+    }
+
+    protected void sourceDetails_Selecting(object sender, ObjectDataSourceSelectingEventArgs e)
+    {
+        if (e.InputParameters[0] == null)
+            e.InputParameters[0] = Guid.Empty;
+            //e.Cancel = true;
+    }
+
+    protected void sourceDetails_Updating(object sender, ObjectDataSourceMethodEventArgs e)
+    {
+        // Add ClientGUID field value to force validation
+        Guid clientGuid = getClientGuidFromQueryString();
+        var asset = (ClientAsset)e.InputParameters[0];
+        asset.ClientGUID = clientGuid;
+    }
+
+    private Guid getClientGuidFromQueryString()
+    {
+        string guidString = Request.QueryString["guid"].ToString();
+        if (guidString != null)
+        {
+            Guid guid = new Guid(guidString);
+            return guid;
+        }
+        return Guid.Empty;
+    }
+
+    protected void gridAsset_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            var item = (ClientAsset)e.Row.DataItem;            
+            totalAssets += item.Amount;
+        } else if (e.Row.RowType == DataControlRowType.Footer)
+        {
+            if (totalAssets > 0)
+            {
+                labelTotalAssets.Text = totalAssets.ToString("C0");
+                labelTotalInvestment.Text = Client.InvestmentAmount.ToString("C0");
+                tableInvestmentSummary.Visible = true;                
+                totalAssets = 0;
+            }
+            
+        } else if (e.Row.RowType == DataControlRowType.EmptyDataRow) {
+            totalAssets = 0;
+            tableInvestmentSummary.Visible = false;
+        }
+    }
+
+    private Client Client
+    {
+        get
+        {
+            if (client == null)
+            {
+                Guid clientGuid = getClientGuidFromQueryString();
+                if (clientGuid != Guid.Empty)
+                    client = Client.GetClientByGUID(clientGuid);
+            }
+            return client;
+        }
+    }
+
+    protected void gridAsset_RowUpdated(object sender, GridViewUpdatedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            labelExceptionDetails.Visible = true;
+            labelExceptionDetails.Text = "There was a problem updating the client assets. ";
+            labelExceptionDetails.Text += "<br/>";
+            labelExceptionDetails.Text += e.Exception.InnerException.Message;
+            e.ExceptionHandled = true;
+            e.KeepInEditMode = true;
         }
 
     }
