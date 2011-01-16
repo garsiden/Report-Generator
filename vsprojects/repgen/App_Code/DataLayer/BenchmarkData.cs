168ca548faaceba7862b4e37bfa39c7ebee253ce
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-16 12:06:08 +0000

    New Dtaa classes. Utility has method to bulk import to database
    git-svn-id: svn://localhost/vantis/repgen/trunk@303 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/App_Code/DataLayer/BenchmarkData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RSMTenon.Data
+{
+    public partial class BenchmarkData
+    {
+        #region Data Validation
+
+
+        partial void OnDateChanging(System.DateTime value)
+        {
+            if (value < new DateTime(1997,1,1) || value > DateTime.Today)
+            {
+                throw new ArgumentException("Date must be less than or equal to today");
+            }
+        }
+
+        #endregion Data Validation
+
+    }
+}
