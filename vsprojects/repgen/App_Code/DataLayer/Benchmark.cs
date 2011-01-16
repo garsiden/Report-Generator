4ee1074a787c55b0dd4ac8a40ae43052a1b99da9
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-16 12:13:12 +0000

    Remove upload code
    git-svn-id: svn://localhost/vantis/repgen/trunk@311 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/App_Code/DataLayer/Benchmark.cs
+++ vsprojects/repgen/App_Code/DataLayer/Benchmark.cs
@@ -11,25 +11,6 @@ namespace RSMTenon.Data
     [System.ComponentModel.DataObject]
     public partial class Benchmark
     {
-        public static void UploadToDatabase(DataUpload.BenchmarkDataTable dt)
-        {
-            string tableName = "tblBenchmarkDataTest";
-            string sql = String.Format("DELETE FROM {0}", tableName);
-
-            using (SqlConnection cn = ConnectionFactory.CreateSqlConnection())
-            {
-                cn.Open();
-                SqlCommand cmd = new SqlCommand(sql, cn);
-                int deleted = cmd.ExecuteNonQuery();
-                Console.WriteLine("{0} record(s) deleted", deleted);
-
-                using (SqlBulkCopy bc = new SqlBulkCopy(cn))
-                {
-                    bc.DestinationTableName = tableName;
-                    bc.WriteToServer(dt);
-                }
-            }
-        }
 
     }
 
