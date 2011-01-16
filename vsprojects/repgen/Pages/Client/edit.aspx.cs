2e4061858a8b3ad3cc9656a434a83399ebc715c4
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-16 12:20:10 +0000

    Uncomment data update error handling code
    git-svn-id: svn://localhost/vantis/repgen/trunk@321 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/Pages/Client/edit.aspx.cs
+++ vsprojects/repgen/Pages/Client/edit.aspx.cs
@@ -17,20 +17,20 @@ public partial class Pages_Client_edit : RepGenPage
 
     protected void formClient_ItemUpdated(object sender, FormViewUpdatedEventArgs e)
     {
-        //if (e.Exception != null)
-        //{
-        //    // Display a user-friendly message
-        //    ExceptionDetails.Visible = true;
-        //    ExceptionDetails.Text = "There was a problem updating the client. ";
-        //    ExceptionDetails.Text += "<br/>";
-        //    ExceptionDetails.Text += e.Exception.Message;
+        if (e.Exception != null)
+        {
+            // Display a user-friendly message
+            ExceptionDetails.Visible = true;
+            ExceptionDetails.Text = "There was a problem updating the client. ";
+            ExceptionDetails.Text += "<br/>";
+            ExceptionDetails.Text += e.Exception.Message;
 
-        //    // Indicate that the exception has been handled
-        //    e.ExceptionHandled = true;
+            // Indicate that the exception has been handled
+            e.ExceptionHandled = true;
 
-        //    // Keep the row in edit mode
-        //    e.KeepInEditMode = true;
-        //}
+            // Keep the row in edit mode
+            e.KeepInEditMode = true;
+        }
     }
 
     protected void btnCreateReport_Click(object sender, EventArgs e)
