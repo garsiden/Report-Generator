c171f4463ad52853d7dfad557f516f60a4e697df
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-17 11:55:33 +0000

    Use LinqDataSource fro recent clients
    git-svn-id: svn://localhost/vantis/repgen/trunk@336 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/repgen/Default.aspx.cs
+++ vsprojects/repgen/Default.aspx.cs
@@ -7,8 +7,14 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : RepGenPage
 {
+    private static int recentClients = 10;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        this.gridClient.PageSize = recentClients;
+    }
+    protected void sourceClient_Selecting(object sender, LinqDataSourceSelectEventArgs e)
+    {
+        e.Result = GetRecentClients(recentClients);
     }
 }
