1971fa4f01315803639b552b580dcbde4eda7131
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-21 15:19:20 +0000

    Page to add content for new strategy
    git-svn-id: svn://localhost/vantis/repgen/trunk@380 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/Pages/Content/new.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/SiteMasterPage.master" AutoEventWireup="true" CodeFile="new.aspx.cs" Inherits="Pages_Content_new" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+<h4>Add Content Records For New Strategy</h4>
+    <asp:DropDownList ID="listStrategy" runat="server" DataTextField="Name" DataSource='<%# GetStrategiesWithoutContent() %>'
+        DataValueField="ID" Width="200px">
+    </asp:DropDownList>
+<br />
+<br />
+    <asp:Button ID="buttonAdd" runat="server" Text="Add Records" 
+        onclick="buttonAdd_Click" />
+<br />
+<br />
+    <asp:Label ID="labelStatus" runat="server" Text=""></asp:Label>
+
+</asp:Content>
+
---
+++ vsprojects/repgen/Pages/Content/new.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RSMTenon.Data;
+
+public partial class Pages_Content_new : RepGenPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            this.listStrategy.DataBind();
+        }
+        labelStatus.Text = "";
+    }
+
+    protected void buttonAdd_Click(object sender, EventArgs e)
+    {
+        string strategyId = this.listStrategy.SelectedValue;
+
+        if (strategyId != String.Empty)
+        {
+            try
+            {
+                RSMTenon.Data.Content.AddContentForStrategy(strategyId);
+            } catch (Exception err)
+            {
+                this.listStrategy.Text = err.Message;
+            }
+            labelStatus.Text = "Records added";
+            this.listStrategy.DataBind();
+        } else
+        {
+            labelStatus.Text = "No Strategy to add records";
+        }
+    }
+
+    protected IQueryable<Strategy> GetStrategiesWithoutContent()
+    {
+        return Strategy.GetStrategiesWithoutContent();
+    }
+}
