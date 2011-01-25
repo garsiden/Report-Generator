9c6401cc03233b1c02095215d5b48a8077b32e97
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-25 14:40:19 +0000

    New page to upload content Xml file
    git-svn-id: svn://localhost/vantis/repgen/trunk@404 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/Pages/Content/upload.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Xml;
+
+public partial class Pages_Content_upload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        if (uploader.PostedFile.ContentLength != 0)
+        {
+            try
+            {
+                if (uploader.PostedFile.ContentLength > 1000000)
+                {
+                    lblStatus.Text = "File is too large for upload";
+                } else
+                {
+                    string destPath = Server.MapPath("~/App_Data/content_test.xml");
+
+                    // try opening as Xml file
+                    using (StreamReader sr = new StreamReader(uploader.PostedFile.InputStream))
+                    {
+                        XmlDocument doc = new XmlDocument();
+                        doc.Load(sr);
+                        doc.Save(destPath);
+
+                    }
+                    lblStatus.Text = String.Format("File {0} uploaded", uploader.PostedFile.FileName);
+                }
+            } catch (Exception err)
+            {
+                lblStatus.Text = err.Message;
+            }
+        }
+    }
+}
