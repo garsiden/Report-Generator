68a78ae1e2753c4dfee98cc79f518b3b0c03bafa
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-27 17:34:02 +0000

    Add a CustomLayout to graph to accomodate all Asset Class in allocation chart legend
    git-svn-id: svn://localhost/vantis/repgen/trunk@453 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/RSMTenon.Graphing/AllocationPieChart.cs
+++ vsprojects/RSMTenon.Graphing/AllocationPieChart.cs
@@ -11,12 +11,6 @@ namespace RSMTenon.Graphing
 {
     public class AllocationPieChart : PieGraph
     {
-
-        public  Chart GenerateChart(string title)
-        {
-            return null;
-        }
-
         public Chart GenerateChart(string title, List<AssetWeighting>model)
         {
             Chart chart1 = new Chart();
@@ -96,6 +90,57 @@ namespace RSMTenon.Graphing
             chart1.Append(plotVisibleOnly1);
 
             return chart1;
+        }
+
+        protected override Legend GenerateLegend(LegendPositionValues position)
+        {
+            Legend legend1 = new Legend();
+            LegendPosition legendPosition1 = new LegendPosition() { Val = position };
+
+            Layout layout1 = new Layout();
+
+            // Manual layout to accomodate all Asset Classes
+            ManualLayout manualLayout1 = new ManualLayout();
+            LeftMode leftMode1 = new LeftMode() { Val = LayoutModeValues.Edge };
+            TopMode topMode1 = new TopMode() { Val = LayoutModeValues.Edge };
+            Left left1 = new Left() { Val = 0.74094630872483225D };
+            Top top1 = new Top() { Val = 0.13422099673202617D };
+            Width width1 = new Width() { Val = 0.24484787472035793D };
+            Height height1 = new Height() { Val = 0.84387581699346426D };
+
+            manualLayout1.Append(leftMode1);
+            manualLayout1.Append(topMode1);
+            manualLayout1.Append(left1);
+            manualLayout1.Append(top1);
+            manualLayout1.Append(width1);
+            manualLayout1.Append(height1);
+
+            layout1.Append(manualLayout1);
+
+            TextProperties textProperties1 = new TextProperties();
+            A.BodyProperties bodyProperties1 = new A.BodyProperties();
+            A.ListStyle listStyle1 = new A.ListStyle();
+
+            A.Paragraph paragraph1 = new A.Paragraph();
+
+            A.ParagraphProperties paragraphProperties1 = new A.ParagraphProperties();
+            A.DefaultRunProperties defaultRunProperties1 = new A.DefaultRunProperties() { Language = "en-GB" };
+
+            paragraphProperties1.Append(defaultRunProperties1);
+            A.EndParagraphRunProperties endParagraphRunProperties1 = new A.EndParagraphRunProperties() { Language = "en-US" };
+
+            paragraph1.Append(paragraphProperties1);
+            paragraph1.Append(endParagraphRunProperties1);
+
+            textProperties1.Append(bodyProperties1);
+            textProperties1.Append(listStyle1);
+            textProperties1.Append(paragraph1);
+
+            legend1.Append(legendPosition1);
+            legend1.Append(layout1);
+            legend1.Append(textProperties1);
+
+            return legend1;
         }
     }
 }
