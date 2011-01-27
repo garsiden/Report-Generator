f16563350c9165ef53a6d67f45642879d2bfc35c
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-01-27 12:03:37 +0000

    Remove unused reference
    git-svn-id: svn://localhost/vantis/repgen/trunk@426 f059b2e8-4df0-46e1-8ac8-86f86453394b

--- vsprojects/RSMTenon.Graphing/StressTestBarChart.cs
+++ vsprojects/RSMTenon.Graphing/StressTestBarChart.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Data.Linq;
 using System.Text;
 using DocumentFormat.OpenXml.Drawing.Charts;
 using A = DocumentFormat.OpenXml.Drawing;
