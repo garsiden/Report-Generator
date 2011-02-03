a3d2786ea78b1fc2ede5e20ea8dae2cc015d3c32
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-02-03 12:56:26 +0000

    JavaScript library
    git-svn-id: svn://localhost/vantis/repgen/trunk@479 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/scripts/repgen.js
@@ -0,0 +1,9 @@
+﻿// hide exception label if previous error is being displayed
+function hideException(controlId) {
+    var fullname = "ctl00_ContentPlaceHolder1_".concat(controlId);
+    var label = document.getElementById(fullname);
+
+    if (label != null) {
+        label.innerText = "";
+    }
+}
