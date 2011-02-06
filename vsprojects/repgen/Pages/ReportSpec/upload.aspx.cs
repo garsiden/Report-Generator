9834fb6989a8808a104706567b14ebf9e925f533
garsiden <garsiden@f059b2e8-4df0-46e1-8ac8-86f86453394b>
2011-02-06 16:41:08 +0000

    Allow uploading of report-spec xml file
    git-svn-id: svn://localhost/vantis/repgen/trunk@507 f059b2e8-4df0-46e1-8ac8-86f86453394b

---
+++ vsprojects/repgen/Pages/ReportSpec/upload.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Xml;
+
+public partial class Pages_ReportSpec_upload : System.Web.UI.Page
+{
+    private static string spec = "report-spec.xml";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        if (uploader.PostedFile.ContentLength != 0) {
+            try {
+                if (uploader.PostedFile.ContentLength > 1000000) {
+                    lblStatus.Text = "File is too large for upload";
+                } else {
+                    string destPath = Server.MapPath(String.Format("~/App_Data/{0}", spec));
+
+                    // try opening as Xml file
+                    using (StreamReader sr = new StreamReader(uploader.PostedFile.InputStream)) {
+                        XmlDocument doc = new XmlDocument();
+                        doc.Load(sr);
+                        doc.Save(destPath);
+                    }
+                    lblStatus.Text = String.Format("File {0} uploaded", uploader.PostedFile.FileName);
+                }
+            } catch (Exception err) {
+                lblStatus.Text = err.Message;
+            }
+        }
+    }
+}
