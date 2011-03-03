434138910b3de0eb878c022b8c5432f5f1489f52
Nigel Garside <nigel.garside@gmail.com>
2011-03-03 12:16:12 +0000

    Get User from HttpContext and display roles

--- vsprojects/repgen/UserControls/SiteHeader.ascx.cs
+++ vsprojects/repgen/UserControls/SiteHeader.ascx.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Security.Principal;
 
 public partial class UserControls_SiteHeader : System.Web.UI.UserControl
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (!IsPostBack)
-            this.greetingLabel.Text = String.Format("User: {0}", RSMTenon.ReportGenerator.ReportGenerator.GetUserId());
-
+        if (!IsPostBack) {
+            var principal = (WindowsIdentity)HttpContext.Current.User.Identity;
+            this.greetingLabel.Text = String.Format("User: {0}", principal.Name.Split('\\')[1]);
+            foreach (var group in principal.Groups)
+                greetingLabel.Text += String.Format("<br />{0}", group.Translate(typeof(NTAccount)));
+        }
     }
 }
