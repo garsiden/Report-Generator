93e16c580c429592c19a0a251a75b9f147330716
Nigel Garside <nigel.garside@gmail.com>
2011-04-07 18:10:42 +0100

    Method to add external data to ChartSpace

--- vsprojects/RSMTenon.Graphing/GraphSpace.cs
+++ vsprojects/RSMTenon.Graphing/GraphSpace.cs
@@ -32,25 +32,36 @@ namespace RSMTenon.Graphing
             // c:lang (EditingLanguage)
             C.EditingLanguage editingLanguage1 = new C.EditingLanguage() { Val = Graph.DEFAULT_LANG };
 
-            //// c:printSettings (PrintSettings)
-            //C.PrintSettings printSettings1 = new C.PrintSettings();
-            //C.HeaderFooter headerFooter1 = new C.HeaderFooter();
-            //C.PageMargins pageMargins1 = new C.PageMargins() { Left = 0.70D, Right = 0.70D, Top = 0.75D, Bottom = 0.75D, Header = 0.30D, Footer = 0.30D };
-            //C.PageSetup pageSetup1 = new C.PageSetup();
-
-            //printSettings1.Append(headerFooter1);
-            //printSettings1.Append(pageMargins1);
-            //printSettings1.Append(pageSetup1);
-
             if (date1904 != null) {
                 chartSpace1.Append(date1904);
             }
             chartSpace1.Append(editingLanguage1);
             chartSpace1.Append(chart);
-            //chartSpace1.Append(printSettings1);
 
             return chartSpace1;
         }
+
+        public static C.ChartSpace GenerateChartSpaceWithData(C.Chart chart, string externalDataId)
+        {
+            // c:chartSpace (ChartSpace)            
+            C.ChartSpace chartSpace1 = new C.ChartSpace();
+            chartSpace1.AddNamespaceDeclaration("c", "http://schemas.openxmlformats.org/drawingml/2006/chart");
+            chartSpace1.AddNamespaceDeclaration("a", "http://schemas.openxmlformats.org/drawingml/2006/main");
+            chartSpace1.AddNamespaceDeclaration("r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+
+            // c:lang (EditingLanguage)
+            C.EditingLanguage editingLanguage1 = new C.EditingLanguage() { Val = Graph.DEFAULT_LANG };
+
+            C.ExternalData externalData1 = new C.ExternalData() { Id = externalDataId };
+
+            chartSpace1.Append(editingLanguage1);
+            chartSpace1.Append(chart);
+            chartSpace1.Append(externalData1);
+
+            return chartSpace1;
+        }
+
+
 
         // Creates an Date1904 instance and adds its children.
         private static C.Date1904 generateDate1904()
