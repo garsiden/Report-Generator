52bb1aa0f83089d2f496400239d4d790ae212466
Nigel Garside <nigel.garside@gmail.com>
2011-04-12 14:31:58 +0100

    Remove unused namespace

--- vsprojects/RSMTenon.Graphing/DrawdownLineChart.cs
+++ vsprojects/RSMTenon.Graphing/DrawdownLineChart.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DocumentFormat.OpenXml.Drawing.Charts;
-using A = DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml;
 
 namespace RSMTenon.Graphing
