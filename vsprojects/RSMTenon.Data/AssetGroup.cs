3e64a263c77ca8aa3e7be62b536da7f740d08c1f
Nigel Garside <nigel.garside@gmail.com>
2011-05-24 14:45:46 +0100

    Add new AssetGroup class and OpenXml dll

---
+++ vsprojects/RSMTenon.Data/AssetGroup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSMTenon.Data
+{
+    [System.ComponentModel.DataObject]
+    public partial class AssetGroup
+    {
+        public static string GetAssetGroupNameFromId(string id)
+        {
+            var ctx = new RepGenDataContext();
+            var asset = ctx.AssetGroups.SingleOrDefault(a => a.ID == id);
+            if (asset != null)
+                return asset.Name;
+            else
+                return null;
+        }
+
+        public static IQueryable<AssetGroup> GetAssetGroups()
+        {
+            var ctx = new RepGenDataContext();
+            return ctx.AssetGroups;
+        }
+    }
+}
